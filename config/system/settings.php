4796385232e85d8efd1bfe342269724a4397e576
chris-at-github <pschorr.christian@gmail.com>
2026-07-22 16:45:41 +0200

    Installation praetorius/vite-asset-collector

--- config/system/settings.php
+++ config/system/settings.php
@@ -31,6 +31,11 @@ return [
             'telephoneValidationPatternForJs' => '/[^\\d\\+\\s\\-]/g',
             'telephoneValidationPatternForPhp' => '/[^\\d\\+\\s\\-]/',
         ],
+        'vite_asset_collector' => [
+            'defaultManifest' => '_assets/vite/.vite/manifest.json',
+            'devServerUri' => 'auto',
+            'useDevServer' => 'auto',
+        ],
     ],
     'FE' => [
         'cacheHash' => [
